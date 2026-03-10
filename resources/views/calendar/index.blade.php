cb4df38ec047cea59d404918a460e2d50cc9f2f5
eugeniogiusti <eugenio.giusti@outlook.it>
2026-03-10 18:20:16 +0100

    mod on calendar

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <iframe 
-        src="https://calendar.google.com/calendar/embed?src=aige9313%40gmail.com&ctz=Europe%2FRome" 
+        src="https://calendar.google.com/calendar/embed?src=your%40gmail.com&ctz=Europe%2FRome" 
         style="width: 100%; height: calc(100vh - 130px); border: none;"
         frameborder="0" 
         scrolling="no"
