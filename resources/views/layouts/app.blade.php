124a2f890669e7123c7705d00f49134aa7dfd281
eugeniogiusti <eugenio.giusti@outlook.it>
2026-04-06 12:30:40 +0200

    updated version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'IndieDesk') }}</title>
     <link rel="icon" href="{{ asset('images/logo.ico') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
